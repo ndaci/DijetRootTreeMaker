1f17e126de54d98c9b945e86557018109220fc1e
Francesco Santanastasio <francesco.santanastasio@cern.ch>
2015-09-08 11:41:30 +0200

    added SingleMuon input files for production

---
+++ prod/submitJobsWithCrab3/Inputs_SingleMuon/crab3_template_data.py
@@ -0,0 +1,32 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.General.workArea = 'WORKINGAREA'
+config.General.requestName = 'WORKINGDIR'
+config.section_('JobType')
+config.JobType.psetName = 'CMSSWCFG'
+config.JobType.pluginName = 'Analysis'
+config.JobType.outputFiles = ['OUTFILENAME']
+config.section_('Data')
+config.Data.inputDataset = 'INPUTDATASET'
+config.Data.unitsPerJob = FILESPERJOB #without '' since it must be an int
+config.Data.splitting = 'LumiBased'
+config.Data.publication = False
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-248005_13TeV_PromptReco_Collisions15_ZeroTesla_JSON_CaloOnly.txt'
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/DCSOnly/json_DCSONLY_Run2015B.txt'
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-255031_13TeV_PromptReco_Collisions15_50ns_JSON.txt'
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_254833_13TeV_PromptReco_Collisions15_JSON.txt'
+#config.Data.lumiMask = 'json_JetHT17Jul_missingLumiSummary.json' ## if you downloaded the file in the working directory
+#config.Data.runRange = '251244-251585' # '193093-194075'                             
+#config.Data.runRange = '251586-251883' # '193093-194075'                             
+##
+#config.Data.outLFNDirBase = '/store/group/phys_exotica/dijet/Dijet13TeV/santanas/rootTrees/Run2015B_SingleMuonPromptReco_08Sep2015-CertJson50ns-XXXXXX-YYYYYY_JEC-Summer15_50nsV4_0d7d556/'
+config.Data.outLFNDirBase = '/store/group/phys_exotica/dijet/Dijet13TeV/santanas/rootTrees/Run2015C_SingleMuonPromptReco_08Sep2015-CertJson50ns-XXXXXX-YYYYYY_JEC-Summer15_50nsV4_0d7d556/'
+#config.Data.outLFNDirBase = '/store/user/santanas/rootTrees/Run2015B_SingleElectron_07Aug2015-CertJson-251586-251883_JEC-Summer15_50nsV2_501dfb2/'
+##
+config.section_('User')
+config.section_('Site')
+config.Site.storageSite = 'T2_CH_CERN'
+#config.Site.storageSite = 'T2_IT_Rome'
