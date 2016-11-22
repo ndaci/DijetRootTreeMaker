f484b6da02b87892a1e170e93ab4d986a4519b01
federico de guio <federico.de.guio@cern.ch>
2016-11-22 11:54:23 +0100

    commit changes to crab files for the record

--- prod/submitJobsWithCrab3/crab3_template_data.py
+++ prod/submitJobsWithCrab3/crab3_template_data.py
@@ -14,11 +14,11 @@ config.Data.inputDataset = 'INPUTDATASET'
 config.Data.unitsPerJob = FILESPERJOB #without '' since it must be an int
 config.Data.splitting = 'LumiBased'
 config.Data.publication = False
-#config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/DCSOnly/json_DCSONLY.txt'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/DCSOnly/json_DCSONLY.txt'
 #config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_254833_13TeV_PromptReco_Collisions15_JSON.txt'
-config.Data.lumiMask = 'JSON_Certified/Cert_271036-283685_13TeV_PromptReco_Collisions16_JSON_NoL1T_28Oct16.txt'
+#config.Data.lumiMask = 'JSON_Certified/Cert_271036-278808_13TeV_PromptReco_Collisions16_JSON_NoL1T_19Aug16.txt'
 #config.Data.runRange = '254833-254833'#'208306-238354' # '193093-194075'                             
-config.Data.outLFNDirBase = '/store/group/phys_exotica/dijet/Dijet13TeV/juska/moriond16_big_cert_v0/'
+config.Data.outLFNDirBase = '/store/group/phys_exotica/dijet/Dijet13TeV/deguio/fall16_big_cert/Full2016_20161117/'
 #config.Data.outLFNDirBase = '/store/user/santanas/rootTrees/Run2015B_JetHT_10June2015DCSJson_JECV5_5a70fc3/'
 config.section_('User')
 config.section_('Site')
